e6b48b715a534548a39d3cc6bbd45d00aa9ba896
georgeatr <61525971+georgeatr@users.noreply.github.com>
2020-08-19 22:28:35 -0400

    Final Build Upload

---
+++ app/src/test/java/com/finalproject/tasteoftheworld/MainActivityTest.groovy
@@ -0,0 +1,18 @@
+package com.finalproject.tasteoftheworld
+
+class MainActivityTest extends groovy.util.GroovyTestCase {
+    void testOnCreate() {
+    }
+
+    void testOnClickCanada() {
+    }
+
+    void testOnClickJapan() {
+    }
+
+    void testOnClickMexico() {
+    }
+
+    void testOnClickGuyana() {
+    }
+}
